8a9b965da80c2a54704be6b2c50bce58ccaf99e7
ShineiArakawa <arakawashinei1115@gmail.com>
2023-08-17 03:38:06 +0900

    Remove cmake dir.

--- cmake/macros/macro_to_string.cmake
+++
@@ -1,8 +0,0 @@
-MACRO(TO_STRING _variable)
-  SET(${_variable} "")
-  FOREACH(_var  ${ARGN})
-    SET(${_variable} "${${_variable}} ${_var}")
-  ENDFOREACH()
-  STRING(STRIP "${${_variable}}" ${_variable})
-ENDMACRO()
-
