f7b5cf82356826d2a0e40b2bb1bd029091dc3524
Shinei Arakawa <arakawashinei1115@gmail.com>
2024-02-09 07:39:17 +0900

    Add source codes.

---
+++ src/App/ViewerGUI/ImGui.cpp
@@ -0,0 +1,69 @@
+#include <App/ImGui.hpp>
+#include <iostream>
+
+namespace gui {
+FrameBuffer::FrameBuffer(float width, float height) {
+  glGenFramebuffers(1, &_fbo);
+  glBindFramebuffer(GL_FRAMEBUFFER, _fbo);
+
+  glGenTextures(1, &_texture);
+  glBindTexture(GL_TEXTURE_2D, _texture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture, 0);
+
+  glGenRenderbuffers(1, &_rbo);
+  glBindRenderbuffer(GL_RENDERBUFFER, _rbo);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbo);
+
+  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glBindTexture(GL_TEXTURE_2D, 0);
+  glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+  _width = width;
+  _height = height;
+}
+
+FrameBuffer::~FrameBuffer() {
+  glDeleteFramebuffers(1, &_fbo);
+  glDeleteTextures(1, &_texture);
+  glDeleteRenderbuffers(1, &_rbo);
+}
+
+unsigned int FrameBuffer::getFrameTexture() {
+  return _texture;
+}
+
+void FrameBuffer::RescaleFrameBuffer(float width, float height) {
+  glBindFramebuffer(GL_FRAMEBUFFER, _fbo);
+
+  glBindTexture(GL_TEXTURE_2D, _texture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _texture, 0);
+
+  glBindRenderbuffer(GL_RENDERBUFFER, _rbo);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbo);
+
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+  _width = width;
+  _height = height;
+}
+
+void FrameBuffer::Bind() const {
+  glBindFramebuffer(GL_FRAMEBUFFER, _fbo);
+}
+
+void FrameBuffer::Unbind() const {
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+}  // namespace gui
