12099ca6a37171f47bc242e72c485c10f3a4b677
ShineiArakawa <arakawashinei1115@gmail.com>
2024-03-10 05:19:39 +0900

    Update source codes.

--- src/Main/ViewerGUI/main.cpp
+++ src/Main/ViewerGUI/main.cpp
@@ -1,13 +1,29 @@
 #include <App/ViewerGUIApp.hpp>
 
-#if defined(WIN32) && defined(ENABLE_LEAK_CHECK)
+#if defined(WIN32)
+#include <Windows.h>
+
+#if defined(ENABLE_LEAK_CHECK)
+// Memory leak checker for MSVC
 #include <crtdbg.h>
 #endif  // ENABLE_LEAK_CHECK
 
+#endif  // WIN32
+
 using namespace simview::app;
 
+#if defined(WIN32)
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow) {
+#else
 int main(int argc, char** argv) {
+#endif
+
 #if defined(WIN32) && defined(ENABLE_LEAK_CHECK)
+  // Initialize memory leak checker for MSVC
   _CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
 #endif  // ENABLE_LEAK_CHECK
 
